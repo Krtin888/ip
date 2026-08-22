0b6fdc4c40a95f70030fbc1db84c3f09e49997a3
Krtin888 <krtin888@gmail.com>
2026-08-23 01:30:50 +0530

    Add in-memory task storage and listing
    Store up to 100 task descriptions for the current session and provide a numbered list so users can review what they entered.

--- src/main/java/Chris.java
+++ src/main/java/Chris.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
  */
 public class Chris {
     private static final String SEPARATOR = "____________________________________________________________";
+    private static final int MAX_TASKS = 100;
 
     /**
      * Greets the user and exits when the user enters {@code bye}.
@@ -13,6 +14,8 @@ public class Chris {
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
 
         System.out.println(SEPARATOR);
         System.out.println(" Hello! I'm Chris");
@@ -26,11 +29,20 @@ public class Chris {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println(SEPARATOR);
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(SEPARATOR);
+                System.out.println(" Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(SEPARATOR);
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println(SEPARATOR);
+                System.out.println(" added: " + input);
+                System.out.println(SEPARATOR);
             }
-
-            System.out.println(SEPARATOR);
-            System.out.println(" " + input);
-            System.out.println(SEPARATOR);
         }
     }
 }
